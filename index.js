f80e553aeffb44e789093ac8d356e8af313f8ded
peloqunt <38419709+peloqunt@users.noreply.github.com>
2019-04-04 16:31:40 -0400

    updated for css

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
 // Maybe we have to have them seperately and got rid of path.
 app.get('/', function(request, response) {
