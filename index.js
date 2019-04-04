65628192b78e68a5feeab96b2a8db2112a5c69e6
peloqunt <38419709+peloqunt@users.noreply.github.com>
2019-04-04 16:21:11 -0400

    css?

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname,'/public'))
+app.use(express.static(__dirname, "public"))
 app.use("/style", express.static(__dirname));
 
 // Maybe we have to have them seperately and got rid of path.
