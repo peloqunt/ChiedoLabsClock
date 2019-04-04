441d8fb3a5c59e1db8acbb13c7f475a983024bd3
peloqunt <38419709+peloqunt@users.noreply.github.com>
2019-04-04 14:41:11 -0400

    Trying to import index.html to heroku app

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname + 'index.html'))
   response.send('Hello World!')
 })
 
